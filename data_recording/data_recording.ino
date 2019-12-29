741816adca01fc93cd99969b9122f8c50742ae3f
luick klippel <gelberdev@gmail.com>
2019-12-29 19:08:25 +0100

    adding arduino data recording sketch

---
+++ data_recording/data_recording.ino
@@ -0,0 +1,42 @@
+#include <Arduino_LSM9DS1.h> //Include the library for 9-axis IMU
+#include <Arduino_LPS22HB.h> //Include library to read Pressure 
+#include <Arduino_HTS221.h> //Include library to read Temperature and Humidity 
+#include <Arduino_APDS9960.h> //Include library for colour, proximity and gesture recognition
+
+void setup()
+{
+  Serial.begin(9600); //Serial monitor to display all sensor values 
+
+  if (!IMU.begin()) //Initialize IMU sensor 
+  { Serial.println("Failed to initialize IMU!"); while (1);}
+
+  if (!BARO.begin()) //Initialize Pressure sensor 
+  { Serial.println("Failed to initialize Pressure Sensor!"); while (1);}
+
+  if (!HTS.begin()) //Initialize Temperature and Humidity sensor 
+  { Serial.println("Failed to initialize Temperature and Humidity Sensor!"); while (1);}
+
+  if (!APDS.begin()) //Initialize Colour, Proximity and Gesture sensor 
+  { Serial.println("Failed to initialize Colour, Proximity and Gesture Sensor!"); while (1);}
+
+  
+  Serial.print("gyro_x,gyro_y,gyro_z,accel_x,accel_y,accel_z");
+ }
+
+float accel_x, accel_y, accel_z;
+float gyro_x, gyro_y, gyro_z;
+
+void loop()
+{
+  //Accelerometer values 
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) 
+  {
+    IMU.readAcceleration(accel_x, accel_y, accel_z);
+    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+    
+    Serial.print(gyro_x); Serial.print(",");Serial.print(gyro_y);Serial.print(",");Serial.println(gyro_z);Serial.print(",");Serial.print(accel_x); Serial.print(",");Serial.print(accel_y);Serial.print(",");Serial.println(accel_z);
+    
+    // Serial.print("Gyroscope = ");Serial.print(gyro_x); Serial.print(", ");Serial.print(gyro_y);Serial.print(", ");Serial.println(gyro_z);
+    // Serial.print("Accelerometer = ");Serial.print(accel_x); Serial.print(", ");Serial.print(accel_y);Serial.print(", ");Serial.println(accel_z);
+  }
+}
