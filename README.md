bbbc6b3b683eba9fb3fbe10fd3261f169553005e
luick klippel <gelberdev@gmail.com>
2020-01-15 20:55:16 +0100

    update readme and add tflite_classi. sketch

--- README.md
+++ README.md
@@ -2,7 +2,31 @@
 
 The Food Watch is a strap (or a watch) which alerts the user when he eats something (via gesture recognition). The strap consists of an Arduino Nano 33 sense which utilizes a tensorflow lite model, which is trained to recognize gestures associated with eating. The training data consists of 3 vector IMU accelerometer and gyro data, with a total of 6 features.
 
-## Neural Net
+## Setup
+
+### Data collection and Training
+
+First you need to collect enough data, you can do that with the `arduino/data_recording` sketch. The serial output is shown in the terminal. To save it just copy and paste it to a file. If you're collecting a lot of data, over a longer period of time you can directly pipe the output of the serial port into a file(best done on linux).
+
+With the collected data you can then start to train your model via. `train.py`.
+If you've trained a model with good enough accuracy, you then have to convert it to a tflite format and next to a c array, so that it can be used on the arduino.
+
+### Convert keras model to tflite model/ c array
+
+To actually run the keras model on the Arduino Nano you need to convert it to an tflite model. To do that, run
+`bash to_tf_lite.sh <keras model path>` and replace the model input/ output name.
+
+To convert the tflite model to a c array, xxd is required, which is preinstalld on most linux distros.
+
+`xxd -i converted_model.tflite > model_data.cc`
+
+### Model deployment on arduino
+
+To run your model on the arduino, move the generated c array model to the `aruino/tflite_classification` sketch and compile&upload the sketch.
+
+Further resources can be found [here](https://www.tensorflow.org/lite/microcontrollers/library)
+
+## Neural Net Choice
 
 ### RNN
 
@@ -13,24 +37,6 @@ Additionally the by the RNN required tensorflow operation that are not supported
 
 A CNN might not be ideal for squential data recognition but is perfect to be used as a sliding window and, continous sequenced data flows.
 You can fin further information on that topic [here](https://medium.com/@jon.froiland/convolutional-neural-networks-for-sequence-processing-part-1-420dd9b500).
-
-## TFLite conversion
-
-### Convert keras model to tflite model
-
-To actually run the keras model on the Arduino Nano you need to convert it to an tflite model. To do that, run
-`bash to_tf_lite.sh <keras model path>` and replace the model input/ output name.
-
-### Convert tflite model to c array
-
-For that xxd is required, which is preinstalld on most linux distros.
-
-`xxd -i converted_model.tflite > model_data.cc`
-
-## Run model on arduino
-
-Resources for that can be found [here](https://www.tensorflow.org/lite/microcontrollers/library)
-
 
 # Depndencies
 
---
+++ arduino/tflite_classification/tflite_classification.ino
@@ -0,0 +1,88 @@
+#include <Arduino_LSM9DS1.h>
+
+#include <Arduino_LSM9DS1.h> //Include the library for 9-axis IMU
+#include <Arduino_LPS22HB.h> //Include library to read Pressure 
+#include <Arduino_HTS221.h> //Include library to read Temperature and Humidity 
+#include <Arduino_APDS9960.h> //Include library for colour, proximity and gesture recognition
+
+#include <TensorFlowLite.h>
+#include <tensorflow/lite/experimental/micro/kernels/all_ops_resolver.h>
+#include <tensorflow/lite/experimental/micro/micro_error_reporter.h>
+#include <tensorflow/lite/experimental/micro/micro_interpreter.h>
+#include <tensorflow/lite/schema/schema_generated.h>
+#include <tensorflow/lite/version.h>
+
+#include "./222_model.cc"
+
+// global variables used for TensorFlow Lite (Micro)
+tflite::MicroErrorReporter tflErrorReporter;
+
+// pull in all the TFLM ops, you can remove this line and
+// only pull in the TFLM ops you need, if would like to reduce
+// the compiled size of the sketch.
+tflite::ops::micro::AllOpsResolver tflOpsResolver;
+
+const tflite::Model* tflModel = nullptr;
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+// Create a static memory buffer for TFLM, the size may need to
+// be adjusted based on the model you are using
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize];
+
+
+// array to map gesture index to a name
+const char* gestures_array[] = {"punch"};
+
+int num_gests = sizeof(GESTURES) / sizeof(GESTURES[0]);
+
+void setup() {
+  Serial.begin(9600); //Serial monitor to display all sensor values 
+
+  if (!IMU.begin()) //Initialize IMU sensor 
+  { Serial.println("Failed to initialize IMU!"); while (1);}
+
+  if (!BARO.begin()) //Initialize Pressure sensor 
+  { Serial.println("Failed to initialize Pressure Sensor!"); while (1);}
+
+  if (!HTS.begin()) //Initialize Temperature and Humidity sensor 
+  { Serial.println("Failed to initialize Temperature and Humidity Sensor!"); while (1);}
+
+  if (!APDS.begin()) //Initialize Colour, Proximity and Gesture sensor 
+  { Serial.println("Failed to initialize Colour, Proximity and Gesture Sensor!"); while (1);}
+
+  
+  // get the TFL representation of the model byte array
+  tflModel = tflite::GetModel(model);
+  if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
+    Serial.println("Model schema mismatch!");
+    while (1);
+  }
+  
+  // Create an interpreter to run the model
+  tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
+  
+  // Allocate memory for the model's input and output tensors
+  tflInterpreter->AllocateTensors();
+  
+  // Get pointers for the model's input and output tensors
+  tflInputTensor = tflInterpreter->input(0);
+  tflOutputTensor = tflInterpreter->output(0);
+}
+
+float accel_x, accel_y, accel_z;
+float gyro_x, gyro_y, gyro_z;
+
+void loop() 
+{  
+  //Accelerometer values 
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) 
+  {
+    IMU.readAcceleration(accel_x, accel_y, accel_z);
+    IMU.readGyroscope(gyro_x, gyro_y, gyro_z);
+  
+  }
+  
+}
